ff524272e817ec18f2a455f1580d20269519412c
sohag142 <sohag2879@gmail.com>
2026-05-10 21:27:03 +0600

    fix vercel deploy

--- index.js
+++ index.js
@@ -2013,6 +2013,7 @@ app.get('/', (req, res) => {
   res.send('Life Spark is Running!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+module.exports = app;

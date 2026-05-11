d5132db888af88ac4da3561933cb37ccd3e03e2c
sohag142 <sohag2879@gmail.com>
2026-05-11 08:52:18 +0600

    fix and Deploy

--- index.js
+++ index.js
@@ -1984,8 +1984,8 @@ app.patch('/users/premium/:email', async(req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   //  await client.db("admin").command({ ping: 1 });
+   //  console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
